744b28c19eb20cd2cd5517f0716416cbd09636c9
Milt0n <cedric.lood@gmail.com>
2015-07-04 09:35:54 +0200

    part 4, generic views, forms

--- pollapp/polls/urls.py
+++ pollapp/polls/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /polls/
-    url(r'^$', views.index, name='index'),
-    # /polls/10/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # /polls/10/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # /polls/10/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
